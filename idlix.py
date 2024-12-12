344fbaae8dcba51f394d879e254671d12269577b
Sadeli Ali <152187061+sadeli99@users.noreply.github.com>
2024-12-12 17:15:19 +0700

    Update idlix.py

--- idlix.py
+++ idlix.py
@@ -1,27 +1,22 @@
 import requests
 import json
 from crypto_helper import CryptoJsAes, dec
-from urllib.parse import urlparse
 
 class Idlix:
-    BASE_WEB_URL = "https://tv4.idlix.asia/"  # URL dasar untuk API yang digunakan
+    BASE_WEB_URL = "https://tv4.idlix.asia/"  # Ubah dengan URL dasar yang sesuai
+    video_id = None
+    embed_url = None
+
     def __init__(self, video_id):
         self.video_id = video_id
-        self.embed_url = None
-        self.m3u8_url = None
 
     def get_embed_url(self):
-        """
-        Fungsi ini untuk mendapatkan embed URL dari IDLIX dengan menggunakan video_id yang diberikan.
-        """
         if not self.video_id:
             return {
                 'status': False,
                 'message': 'Video ID is required'
             }
-
         try:
-            # Mengirim permintaan POST ke server IDLIX untuk mendapatkan embed URL
             request = requests.post(
                 url=self.BASE_WEB_URL + "wp-admin/admin-ajax.php",
                 data={
@@ -31,10 +26,7 @@ class Idlix:
                     "type": "movie",
                 }
             )
-            
-            # Mengecek apakah request berhasil dan ada embed_url dalam respons
             if request.status_code == 200 and request.json().get('embed_url'):
-                # Mendekripsi embed_url menggunakan CryptoJsAes dan key yang diberikan
                 self.embed_url = CryptoJsAes.decrypt(
                     request.json().get('embed_url'),
                     dec(
@@ -51,63 +43,8 @@ class Idlix:
                     'status': False,
                     'message': 'Failed to get embed URL'
                 }
-
         except Exception as error_get_embed_url:
             return {
                 'status': False,
                 'message': str(error_get_embed_url)
-            }
-
-    def get_m3u8_url(self):
-        """
-        Fungsi ini untuk mendapatkan URL video dalam format M3U8 berdasarkan embed_url yang telah diperoleh.
-        """
-        if not self.embed_url:
-            return {
-                'status': False,
-                'message': 'Embed URL is required'
-            }
-
-        # Mengambil bagian tertentu dari embed_url (seperti ID video atau path)
-        if '/video/' in urlparse(self.embed_url).path:
-            self.embed_url = urlparse(self.embed_url).path.split('/')[2]
-        elif urlparse(self.embed_url).query.split('=')[1]:
-            self.embed_url = urlparse(self.embed_url).query.split('=')[1]
-
-        try:
-            # Mengirim permintaan POST untuk mendapatkan M3U8 URL berdasarkan embed_url
-            request = requests.post(
-                url='https://jeniusplay.com/player/index.php',
-                params={
-                    "data": self.embed_url,
-                    "do": "getVideo"
-                },
-                headers={
-                    "Host": "jeniusplay.com",
-                    "X-Requested-With": "XMLHttpRequest",
-                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                },
-                data={
-                    "hash": self.embed_url,
-                    "r": self.BASE_WEB_URL,
-                },
-            )
-
-            # Mengecek apakah permintaan berhasil dan ada videoSource (M3U8 URL)
-            if request.status_code == 200 and request.json().get('videoSource'):
-                self.m3u8_url = request.json().get('videoSource')
-                return {
-                    'status': True,
-                    'm3u8_url': self.m3u8_url
-                }
-            else:
-                return {
-                    'status': False,
-                    'message': 'Failed to get M3U8 URL'
-                }
-
-        except Exception as error_get_m3u8_url:
-            return {
-                'status': False,
-                'message': str(error_get_m3u8_url)
             }
