224e9e473188f969f9363a495d574aa849418cdf
Sadeli Ali <152187061+sadeli99@users.noreply.github.com>
2024-12-12 17:54:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 
 @app.route('/get_embed_url', methods=['POST'])
 def get_embed_url():
+    # Mengambil data JSON dari body request
     data = request.json
     video_id = data.get('video_id')
+    video_type = data.get('video_type', 'movie')  # Default 'movie' jika tidak ada
 
+    # Memastikan video_id ada
     if not video_id:
         return jsonify({"error": "Missing 'video_id'"}), 400
 
-    idlix_instance = Idlix(video_id)
+    # Membuat instance Idlix dengan video_id dan video_type
+    idlix_instance = Idlix(video_id, video_type)
     result = idlix_instance.get_embed_url()
 
     return jsonify(result)
