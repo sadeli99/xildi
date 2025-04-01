1b9ecd8c38f38c8972ebf1d403f2d1f1f681df33
Sadeli Ali <152187061+sadeli99@users.noreply.github.com>
2025-04-02 03:50:42 +0700

    Update idlix.py

--- idlix.py
+++ idlix.py
@@ -3,7 +3,7 @@ import json
 from crypto_helper import CryptoJsAes, dec
 
 class Idlix:
-    BASE_WEB_URL = "https://vip2.idlixvip.asia/"  # Ubah dengan URL dasar yang sesuai
+    BASE_WEB_URL = "https://tv2.idlixvip.asia/"  # Ubah dengan URL dasar yang sesuai
     video_id = None
     video_type = "movie"  # Default adalah 'movie'
     embed_url = None
